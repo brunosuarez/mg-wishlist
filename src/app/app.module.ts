86920e9a341b28f85a93a1f27b881c54f69d686e
bruno.soares <bruno.soares@db1.com.br>
2022-02-19 19:21:56 -0300

    Finalizando Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { WishlistProductsComponent } from './components/wishlist-products/wishlist-products.component';
+import { HeaderComponent } from './shared/components/templates/header/header.component';
+import { SearchComponent } from './shared/components/templates/search/search.component';
+import { CardComponent } from './shared/components/templates/card/card.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListProductsComponent,
+    WishlistProductsComponent,
+    HeaderComponent,
+    SearchComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/wishlist-products/wishlist-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wishlist-products',
+  templateUrl: './wishlist-products.component.html',
+  styleUrls: ['./wishlist-products.component.css']
+})
+export class WishlistProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
